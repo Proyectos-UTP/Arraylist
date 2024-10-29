5d67e369c2500f92f0ac1e86c6934ef5fed024ba
FBN-64 <falonso2024@gmail.com>
2024-10-29 18:38:20 -0500

    Añadido ejemplo 1
    creación de ArrayList de Alumnos y sus promedio. Posteriormente se hace un promedio del aula y se imprime en consola.

---
+++ Arraylist/src/ExpoPrueba1/NotaAlumno.java
@@ -0,0 +1,13 @@
+package ExpoPrueba1;
+public class NotaAlumno {
+    String nombre;
+    int codigo;
+    double prom;
+
+    public NotaAlumno(String nombre, int codigo, double prom) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.prom = prom;
+    }
+    
+}
